00e736648bcaebe87d4b298cc00c203492d0c986
mminhou <exit19093@gmail.com>
2022-05-27 08:46:39 +0900

    Task: Task application

---
+++ Task/Task/TaskViewController.swift
@@ -0,0 +1,30 @@
+//
+//  TaskViewController.swift
+//  Task
+//
+//  Created by CMM on 2022/05/14.
+//
+
+import UIKit
+
+class TaskViewController: UIViewController {
+  
+  @IBOutlet var label: UILabel!
+  var task: String?
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    label.text = task
+    
+    navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Delete", style: .done, target: self, action: #selector(deleteTask))
+  }
+  
+  @objc func deleteTask() {
+    
+//    let newCount = count - 1
+//
+//    UserDefaults().setValue(newCount, forKey: "count")
+//    UserDefaults().setValue(nil, forKey: "task_\(currentPosition)")
+  }
+}
